e7e3205df3087e7c175639d90cfd26345b2740dc
Manik Sharma <90952302+Wmlster@users.noreply.github.com>
2023-12-13 21:23:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 // your code here
+let btn=document.getElementById("button");
+
+btn.addEventListener("click",event);
+ 
+function event() {
+
+	let text=document.getElementById("url");
+	let txt="";
+	txt+=text.innerText;
+	txt+='?';
+
+
+	let f1=document.getElementById("name");
+	txt+=f1.name;
+	txt+='=';
+
+	txt+=f1.value;
+
+	txt+='&'
+
+	
+	let f2=document.getElementById("year");
+	txt+=f2.name;
+	txt+='=';
+
+	txt+=f2.value;
+
+
+	text.innerHTML=txt;
+	
+}
